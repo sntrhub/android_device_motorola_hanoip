2bdaa0456996e730f965a97b39443882c6e20d24
豪 樹 冬 木 <fuyukihidekii@gmail.com>
2023-11-28 11:27:50 -0300

    hanoip: refactor device makefile
    * introduce all properties removed from twrp makefile there.
    
    Change-Id: Ia0191cdca5b19b5869bf4626a2a90d6df5436d9b
    Signed-off-by: 豪 樹 冬 木 <fuyukihidekii@gmail.com>

--- device.mk
+++ device.mk
@@ -5,15 +5,33 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those AOSP products, most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from common TWRP configuration.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 LOCAL_PATH := device/motorola/hanoip
-# A/B
+
+# A/B: update_engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# A/B: OTA dexopt update_engine hookup
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
+# HAL: Boot Control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
@@ -27,9 +45,11 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libz \
     libcutils
 
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+# GMS partner ID
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Build ID
+BUILD_FINGERPRINT := motorola/hanoip_retail/hanoip:11/S2RIS32.32-20-9-9-2-1/d5533:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="hanoip_retail-user 11 S2RIS32.32-20-9-9-2-1 d5533 release-keys"
